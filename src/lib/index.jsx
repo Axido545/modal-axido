52e07d00fca1cd53b41b2958ee1d83f6b027e31c
axido545 <axido545@gmail.com>
2024-02-09 21:48:55 +0100

    add css

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -5,9 +5,12 @@ export default function ModalAxido() {
     return <main className="main-modal">
         <div className="modal-container">
             <img className="close-btn" src={closeImg} alt="close-btn" />
-            <h1>Modal Titre</h1>
-            <p>Modal texte message</p>
-            <button>Annuler</button><button>Confirmer</button>
+            <h3>Modal Titre</h3>
+            <p className="modal-text">Modal texte message Modal texte message Modal texte message Modal texte message  Modal texte message Modal texte message Modal texte message Modal texte message</p>
+            <div className="btn-validations">
+                <button className="btn-annuler">Annuler</button><button className="btn-confirmer">Confirmer</button>
+            </div>
+
         </div>
 
     </main>
