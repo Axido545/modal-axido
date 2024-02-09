e020b5fa9aecb05af225fd938ab0f63f1debdfbf
axido545 <axido545@gmail.com>
2024-02-09 22:24:47 +0100

    add handleClose

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,17 +1,24 @@
 import "./index.css"
 import closeImg from "./close.png"
+import { useState } from "react"
 
 export default function ModalAxido() {
-    return <main className="main-modal">
-        <div className="modal-container">
-            <img className="close-btn" src={closeImg} alt="close-btn" />
-            <h3>Modal Titre</h3>
-            <p className="modal-text">Modal texte message Modal texte message Modal texte message Modal texte message  Modal texte message Modal texte message Modal texte message Modal texte message</p>
-            <div className="btn-validations">
-                <button className="btn-annuler">Annuler</button><button className="btn-confirmer">Confirmer</button>
+    const [isOpen, setIsOpen] = useState(true)
+
+    const handleClose = () => {
+        setIsOpen(false)
+    }
+    return isOpen ? (
+        < main className="main-modal" >
+            <div className="modal-container">
+                <img className="close-btn" src={closeImg} alt="close-btn" onClick={handleClose} />
+                <h3>Modal Titre</h3>
+                <p className="modal-text">Modal texte message Modal texte message Modal texte message Modal texte message  Modal texte message Modal texte message Modal texte message Modal texte message</p>
+                <div className="btn-validations">
+                    <button className="btn-annuler">Annuler</button><button className="btn-confirmer">Confirmer</button>
+                </div>
+
             </div>
 
-        </div>
-
-    </main>
+        </main >) : null;
 }
