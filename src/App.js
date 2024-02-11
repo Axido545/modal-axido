c1b725c02843803d9c074b9778fee29591ab2869
axido545 <axido545@gmail.com>
2024-02-11 12:55:09 +0100

    props modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import ModalAxido from "./lib";
+// import useState from "react";
+
 
 function App() {
+  // const [isOpen, setIsOpen] = useState(false)
+  const textModal = "Modal texte message Modal texte message Modal texte message Modal texte message  Modal texte message Modal texte message Modal texte message Modal texte message"
+  const handleClose = () => {
+    // setIsOpen(false)
+  }
   return <>
-    <ModalAxido />
+    <ModalAxido textModal={textModal} handleClose={handleClose} />
   </>
 }
 
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,24 +1,13 @@
 import "./index.css"
 import closeImg from "./close.png"
-import { useState } from "react"
 
-export default function ModalAxido() {
-    const [isOpen, setIsOpen] = useState(true)
+export default function ModalAxido({ textModal, handleClose }) {
 
-    const handleClose = () => {
-        setIsOpen(false)
-    }
-    return isOpen ? (
-        < main className="main-modal" >
-            <div className="modal-container">
-                <img className="close-btn" src={closeImg} alt="close-btn" onClick={handleClose} />
-                <h3>Modal Titre</h3>
-                <p className="modal-text">Modal texte message Modal texte message Modal texte message Modal texte message  Modal texte message Modal texte message Modal texte message Modal texte message</p>
-                <div className="btn-validations">
-                    <button className="btn-annuler">Annuler</button><button className="btn-confirmer">Confirmer</button>
-                </div>
-
-            </div>
-
-        </main >) : null;
+    return < main className="main-modal" >
+        <div className="modal-container">
+            <img className="close-btn" src={closeImg} alt="close-btn" onClick={handleClose} />
+            <h3>Modal Titre</h3>
+            <p className="modal-text">{textModal}</p>
+        </div>
+    </main >
 }
