788060d5fa4f729737d0334758729e086ea4c0eb
axido545 <axido545@gmail.com>
2024-02-13 19:57:25 +0100

    delete comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import ModalAxido from "./lib";
-// import useState from "react";
-
 
 function App() {
-  // const [isOpen, setIsOpen] = useState(false)
   const textModal = "Modal texte message Modal texte message Modal texte message Modal texte message  Modal texte message Modal texte message Modal texte message Modal texte message"
   const handleClose = () => {
-    // setIsOpen(false)
   }
   return <>
     <ModalAxido textModal={textModal} handleClose={handleClose} />
