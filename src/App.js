ec1dd8ae42e12d00265c2c682fb23f8e516124e4
axido545 <axido545@gmail.com>
2024-02-14 16:42:39 +0100

    test modal axido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import ModalAxido from "./lib";
+import { useState } from "react";
 
 function App() {
+  const [openModal, setOpenModal] = useState(false)
   const textModal = "Modal texte message Modal texte message Modal texte message Modal texte message  Modal texte message Modal texte message Modal texte message Modal texte message"
   const handleClose = () => {
+    setOpenModal(false)
   }
   return <>
-    <ModalAxido textModal={textModal} handleClose={handleClose} />
+    <button onClick={() => setOpenModal(true)}>Afficher la modal</button>
+    <ModalAxido textModal={textModal} handleClose={handleClose} isOpen={openModal} />
   </>
 }
 
